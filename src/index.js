c44fe660544304e2a7e7b80beea19ccb5e50325b
tomdaniels <thomsondaniels@hotmail.com>
2018-08-23 14:47:51 +1000

    feat: index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export { default as Experiment } from './experiment';
+export { default as Treatment } from './treatment';
+export { default as Control } from './control';
+export { default as Optimizely } from './optimizely';
+export { default as Optimize } from './optimize';
+export { default as ExperimentOpacifier } from './experiment-opacifier';
+export { default as Globals } from './experiment-globals';
+export { default as withExperiment } from './with-experiment';
